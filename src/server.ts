5b4caa41a37fd06f2551fa8ef7af333e6eda99f3
ThiagoDeJesus <99thiagodejesus@gmail.com>
2021-10-06 22:51:03 -0300

    feat: primeiras rotas

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,14 @@
-import express from "express";
+import express, { response } from "express";
 
 const app = express();
+
+app.get("/test", (request, response) => {
+  return response.send("Olá NLW");
+});
+
+app.post("/test-post", () => {
+  return response.send("Olá NLW com Post");
+});
 
 app.listen(3000, () => {
   console.log("server is running");
