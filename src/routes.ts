f183c58d9428c877674c4e362800cf6d628e8657
ThiagoDeJesus <99thiagodejesus@gmail.com>
2021-10-13 18:29:29 -0300

    feat: adiciona feature para listar elogios recebidos ou enviados de um usuario

--- src/routes.ts
+++ src/routes.ts
@@ -5,6 +5,8 @@ import { ensureAdmin } from "./middlewares/ensureAdmin";
 import { AuthenticateUsercontroller } from "./controllers/AuthenticateUserController";
 import { CreateComplimentController } from "./controllers/CreateComplimentController";
 import { ensureAuthenticated } from "./middlewares/ensureAuthenticated";
+import { ListUserSendComplimentsController } from "./controllers/ListUserSendComplimentsController";
+import { ListUserReceiveComplimentsController } from "./controllers/ListUserReceiveComplimentsController";
 
 const router = Router();
 
@@ -12,10 +14,33 @@ const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
 const authenticateUserController = new AuthenticateUsercontroller();
 const createComplimentController = new CreateComplimentController();
+const listUserSendComplimentsService = new ListUserSendComplimentsController();
+const listUserReceiveComplimentsService =
+  new ListUserReceiveComplimentsController();
 
 router.post("/users", createUserController.handle);
-router.post("/tags",ensureAuthenticated , ensureAdmin, createTagController.handle);
+router.post(
+  "/tags",
+  ensureAuthenticated,
+  ensureAdmin,
+  createTagController.handle
+);
 router.post("/login", authenticateUserController.handle);
-router.post("/compliments", ensureAuthenticated, createComplimentController.handle);
+router.post(
+  "/compliments",
+  ensureAuthenticated,
+  createComplimentController.handle
+);
+
+router.get(
+  "/users/compliments/send",
+  ensureAuthenticated,
+  listUserSendComplimentsService.handle
+);
+router.get(
+  "/users/compliments/receive",
+  ensureAuthenticated,
+  listUserReceiveComplimentsService.handle
+);
 
 export { router };
