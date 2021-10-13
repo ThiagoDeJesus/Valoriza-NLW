f7468cf57f9a48b7805b0d1337ba95a91d86c930
ThiagoDeJesus <99thiagodejesus@gmail.com>
2021-10-13 18:47:14 -0300

    feat: adiciona feature para listar tags cadastradas no banco de dados

--- src/routes.ts
+++ src/routes.ts
@@ -7,6 +7,7 @@ import { CreateComplimentController } from "./controllers/CreateComplimentContro
 import { ensureAuthenticated } from "./middlewares/ensureAuthenticated";
 import { ListUserSendComplimentsController } from "./controllers/ListUserSendComplimentsController";
 import { ListUserReceiveComplimentsController } from "./controllers/ListUserReceiveComplimentsController";
+import { ListTagsController } from "./controllers/ListTagsController";
 
 const router = Router();
 
@@ -17,6 +18,7 @@ const createComplimentController = new CreateComplimentController();
 const listUserSendComplimentsService = new ListUserSendComplimentsController();
 const listUserReceiveComplimentsService =
   new ListUserReceiveComplimentsController();
+const listTagsController = new ListTagsController();
 
 router.post("/users", createUserController.handle);
 router.post(
@@ -25,6 +27,7 @@ router.post(
   ensureAdmin,
   createTagController.handle
 );
+router.get("/tags", ensureAuthenticated, listTagsController.handle);
 router.post("/login", authenticateUserController.handle);
 router.post(
   "/compliments",
