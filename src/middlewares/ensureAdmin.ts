a9517c14835cf9f867b53af056e9ac2f0c086fe5
ThiagoDeJesus <99thiagodejesus@gmail.com>
2021-10-13 17:57:55 -0300

    feat: corrige o ensureAdmin

--- src/middlewares/ensureAdmin.ts
+++ src/middlewares/ensureAdmin.ts
@@ -1,11 +1,18 @@
 import { Request, Response, NextFunction } from "express";
+import { getCustomRepository } from "typeorm";
+import { User } from "../entities/User";
+import { UsersRepositories } from "../repositories/UsersRepositories";
 
-export function ensureAdmin(
+export async function ensureAdmin(
   request: Request,
   response: Response,
   next: NextFunction
 ) {
-  const admin = true;
+  const { user_id } = request;
+
+  const usersRepositories = getCustomRepository(UsersRepositories);
+
+  const { admin } = (await usersRepositories.findOne(user_id)) as User;
 
   if (admin) {
     return next();
